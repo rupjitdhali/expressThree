493a0ce2244ba6a0cd9de66003ef5e1fe2db11cd
rupjitdhali <rupjitdhali@gmail.com>
2025-11-20 10:22:31 +0000

    20.11

---
+++ server/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+    try {
+        let a = 10;
+        console.log(a)
+
+        res.status(200).json({ msg: 'Hello CFI', num: a})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: error })
+    }
+})
+
+app.get('/', (req, res) => {
+    try {
+        let a = 10;
+        console.log(a);
+
+        res.status(200).json({ msg: "This is CFI classroom"})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: error })
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server Chalu ho gya http://localhost:${PORT}`);
+})
